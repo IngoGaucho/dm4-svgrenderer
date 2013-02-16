2eae8b8746f6e0d51fd11ba8e76da80a98c30f8b
joernweissenborn <joern.weissenborn@gmail.com>
2013-02-17 00:26:55 +0100

    bei deepamehta verschrieben

--- src/main/resources/web/script/plugin.js
+++ src/main/resources/web/script/plugin.js
@@ -1,6 +1,6 @@
 dm4c.add_plugin("de.deepamehta.svgrenderer", function(){
 
-dm4c.load_script("de.deepmamehta.svgrenderer/script/topicmap_renderers/svgrenderer.js")
+dm4c.load_script("/de.deepamehta.svgrenderer/script/topicmap_renderers/svgrenderer.js")
 
  // === Topicmaps Listeners ===
 
